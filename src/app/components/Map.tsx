b63da64366d0e137af668cde5b974885dffd3af9
karan9992 <karanbhoir987@gmail.com>
2026-07-14 13:39:37 +0530

    Changed marker icon and added to map

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -5,10 +5,17 @@ import {
     Marker,
     Popup,
 } from "react-leaflet";
-import "./LeafletMarker";
+// import "./LeafletMarker";
 import { useMapEvents } from "react-leaflet";
 import { useState } from "react";
+import L from "leaflet";
 
+const treeIcon = new L.Icon({
+    iconUrl: "/axe.png",
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+
+});
 
 export default function Map() {
 
@@ -38,7 +45,20 @@ export default function Map() {
     function MapClick() {
         useMapEvents({
             click(e) {
-                console.log(e.latlng);
+                // console.log(e.latlng);
+
+                const ans = confirm("Do you want to report fallen tree?")
+                if (ans) {
+                    let newReprt = {
+                        id: reports.length,
+                        lat: Number(e.latlng.lat.toFixed(4)),
+                        lng: Number(e.latlng.lng.toFixed(4)),
+                        title: "Tree fallen"
+                    }
+
+                    console.log(newReprt)
+                    setReports(prev=>[...prev,newReprt])
+                }
             },
         });
 
@@ -46,7 +66,7 @@ export default function Map() {
     }
     return (
         <MapContainer
-            center={[19.076, 72.8777]}
+            center={[19.068, 73.002]}
             zoom={13}
             style={{ height: "500px", width: "100%" }}
         >
@@ -59,6 +79,7 @@ export default function Map() {
                 <Marker
                     key={report.id}
                     position={[report.lat, report.lng]}
+                    icon={treeIcon}
                 >
                     <Popup>{report.title}</Popup>
                 </Marker>
